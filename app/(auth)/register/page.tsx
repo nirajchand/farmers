003ae99b85fcae6731d883d9076474a9de3d9f09
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-12-23 21:39:45 +0545

    adding register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import Register from "../_components/register";
+
+export default function Page() {
+    return (
+        <div className="flex justify-center pt-15">
+            <Register/>
+        </div>
+    );
+}
