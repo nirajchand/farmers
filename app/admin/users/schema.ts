d461420f49fbbbd16f187f7bcd9e5ccb52b2ad88
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-03 08:22:49 +0545

    adding schema for user create

---
+++ app/admin/users/schema.ts
@@ -0,0 +1,19 @@
+import z, { email } from "zod";
+
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+
+export const userSchema = z.object({
+  fullName: z.string(),
+  email: z.email({ message: "Enter valid email" }),
+  password: z.string().min(6, { message: "min 6 characters" }),
+  confirmPassword: z.string().min(6, { message: "min 6 characters" }),
+  role: z.string(),
+  profile_image: z
+    .instanceof(File)
+    .optional()
+    .refine((file) => !file || ACCEPTED_IMAGE_TYPES.includes(file.type), {
+      message: "Only .jpg, .jpeg, .png and .webp formats are supported",
+    }),
+});
+
+export type UserSchema = z.infer<typeof userSchema>
